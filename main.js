ba31f133846c5f9bb19e77625ddf8786002690df
Ash Luchowa <ashwinluchowa@gmail.com>
2025-04-04 19:00:41 +0800

    Add back to top function

--- main.js
+++ main.js
@@ -49,3 +49,29 @@ window.addEventListener('scroll', () => {
         scrollAppearOther(section);
     });
 });
+
+
+//BACK TO TOP SCROLL
+const showOnPx = 100;
+const topBtn = document.querySelector('.back-top');
+
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener('scroll', () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+        topBtn.style.opacity = '1';
+    } else {
+        topBtn.style.opacity = '0';
+    }
+});
+
+//smooth scroll
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: 'smooth',
+    });
+};
+
+topBtn.addEventListener('click', goToTop);
